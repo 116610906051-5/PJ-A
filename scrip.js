cd1723246c14b397c76be801d85037cdd2fbc03e
116610906051-5 <156157127+116610906051-5@users.noreply.github.com>
2024-02-11 00:20:01 +0700

    สร้างเมนู

---
+++ scrip.js
@@ -0,0 +1,14 @@
+$('.tab').click(function () {
+    $('.tabopen').removeClass('tabopen');
+    $(this).addClass('tabopen');
+});
+
+/*
+
+Android Tabs Created by Niall Paterson.
+
+Twitter: @_niall
+
+Didn't find anything online that provided something like this so I decided to do it. If I had a bit more CSS expertise I'd prefer if it was less 'hackish'. Its not the best way to do this.. If anyone can do this better please tell me on twitter :)
+
+*/
